36a239bf5166465c7847235397723a98e5de03a5
Matthew Shrider <mattshrider@gmail.com>
2023-03-11 15:16:17 -0500

    feat: Use mason to install dap adapters

--- lua/josean/plugins-setup.lua
+++ lua/josean/plugins-setup.lua
@@ -206,6 +206,7 @@ return packer.startup(function(use)
 
   --debugging
   use("mfussenegger/nvim-dap")
+  use("jay-babu/mason-nvim-dap.nvim")
   use("nvim-telescope/telescope-dap.nvim")
   use({ "theHamsta/nvim-dap-virtual-text", requires = { "mfussenegger/nvim-dap" } })
   use({ "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } })
