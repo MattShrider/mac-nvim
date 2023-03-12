723d767f03ce1cb46bc15fbddd8c8cffc3c5a04d
Matthew Shrider <mattshrider@gmail.com>
2023-03-12 17:21:09 -0400

    fix: Use automatic lspconfigs
    Instead of manually setting up each lsp, we can use
    mason to install an LSP, and have a default function
    attach our capabilities and keybindings.
    
    A default can be overridden with a callback function,
    and we override eslint, typescript, and rust right now.

--- lua/josean/plugins/lsp/mason.lua
+++ lua/josean/plugins/lsp/mason.lua
@@ -1,4 +1,10 @@
 -- import mason plugin safely
+local lstatus, lspconfig = pcall(require, "lspconfig")
+if not lstatus then
+  return
+end
+
+-- import mason plugin safely
 local mason_status, mason = pcall(require, "mason")
 if not mason_status then
   return
@@ -16,6 +22,20 @@ if not mason_null_ls_status then
   return
 end
 
+-- import cmp-nvim-lsp plugin safely
+local cmp_nvim_lsp_status, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+if not cmp_nvim_lsp_status then
+  return
+end
+
+-- Change the Diagnostic symbols in the sign column (gutter)
+-- (not in youtube nvim video)
+local signs = { Error = " ", Warn = " ", Hint = "ﴞ ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
+
 -- enable mason
 mason.setup()
 
@@ -28,9 +48,114 @@ mason_lspconfig.setup({
     "tailwindcss",
     "lua_ls",
     "emmet_ls",
+    "gopls",
   },
   -- auto-install configured servers (with lspconfig)
   automatic_installation = true, -- not the same as ensure_installed
+})
+
+-- Automatic lsp configuration
+local keymap = vim.keymap -- for conciseness
+
+-- enable keybinds only for when lsp server available
+local on_attach = function(client, bufnr)
+  -- keybind options
+  local opts = { noremap = true, silent = true, buffer = bufnr }
+
+  -- set keybinds
+  keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", opts) -- show definition, references
+  keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts) -- got to declaration
+  keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts) -- see definition and make edits in window
+  keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts) -- go to implementation
+  keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
+  keymap.set("n", "<leader>.", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
+  keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
+  keymap.set("n", "<leader>D", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- show  diagnostics for line
+  keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts) -- show diagnostics for cursor
+  keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts) -- jump to previous diagnostic in buffer
+  keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- jump to next diagnostic in buffer
+  keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
+  keymap.set("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts) -- see outline on right hand side
+
+  -- typescript specific keymaps (e.g. rename file and update imports)
+  if client.name == "tsserver" then
+    keymap.set("n", "<leader>rf", ":TypescriptRenameFile<CR>") -- rename file and update imports
+    keymap.set("n", "<leader>oi", ":TypescriptOrganizeImports<CR>") -- organize imports (not in youtube nvim video)
+    keymap.set("n", "<leader>ru", ":TypescriptRemoveUnused<CR>") -- remove unused variables (not in youtube nvim video)
+  end
+end
+
+local capabilities = cmp_nvim_lsp.default_capabilities()
+
+mason_lspconfig.setup_handlers({
+  -- The first entry (without a key) will be the default handler
+  -- and will be called for each installed server that doesn't have
+  -- a dedicated handler.
+  function(server_name) -- default handler (optional)
+    lspconfig[server_name].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
+  end,
+
+  -- Next, you can provide a dedicated handler for specific servers.
+  -- For example, a handler override for the `rust_analyzer`:
+  ["rust_analyzer"] = function()
+    local ready, rust = pcall(require, "rust-tools")
+    if not ready then
+      return
+    end
+    rust.setup({
+      server = {
+        capabilities = capabilities,
+        on_attach = on_attach,
+      },
+    })
+  end,
+
+  ["tsserver"] = function()
+    local typescript_setup, typescript = pcall(require, "typescript")
+    if not typescript_setup then
+      return
+    end
+
+    typescript.setup({
+      server = {
+        capabilities = capabilities,
+        on_attach = on_attach,
+      },
+    })
+  end,
+
+  ["emmet_ls"] = function()
+    lspconfig["emmet_ls"].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+      filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+    })
+  end,
+
+  ["lua_ls"] = function()
+    lspconfig["lua_ls"].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+      settings = { -- custom settings for lua
+        Lua = {
+          -- make the language server recognize "vim" global
+          diagnostics = {
+            globals = { "vim" },
+          },
+          workspace = {
+            -- make language server aware of runtime files
+            library = {
+              [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+              [vim.fn.stdpath("config") .. "/lua"] = true,
+            },
+          },
+        },
+      },
+    })
+  end,
 })
 
 mason_null_ls.setup({
